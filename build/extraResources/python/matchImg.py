54acf3bde9bdddab0f825cfadbf2c17315a2cbeb
pengjianming <peng.jianming@hbtown.com>
2026-01-21 17:08:26 +0800

    fix

--- build/extraResources/python/matchImg.py
+++ build/extraResources/python/matchImg.py
@@ -167,30 +167,50 @@ def opencv颜色偏色找图(large_image_path, small_image_path, color_tolerance
 
     # 将大图二值结果也转换为 0/1 掩码
     search_mask = (search_binary_combined == 255).astype(np.uint8)
-    # cv2.imshow('small_binary_combined Threshold', small_binary_combined)
-    # cv2.imshow('search_binary_combined Threshold', search_binary_combined)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+
     # 使用 TM_CCORR 对两个 0/1 掩码做匹配
     # 对于 0/1 掩码，TM_CCORR 的结果等于滑动窗口内 search_mask * template_mask 的和，
     result = cv2.matchTemplate(search_mask, template_mask, cv2.TM_CCORR)
 
+    
     # 找到重合白点最多的位置
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
-    # 自定义相似度：重合白点数 / 模板白点总数，范围[0,1]
-    overlap_white = max_val
+
     white_points = int(np.sum(template_mask))
-    custom_similarity = overlap_white / white_points if white_points > 0 else 0
+    参数相似度 = 0.80
+    # 相似度过关了,就不关注重合白点个数了, 后续关注区域内白点个数相似度, 找到最合适的点
+    if((max_val / white_points) < 参数相似度):
+        return None
 
-    print(f"自定义白点匹配率 - 重合白点: {overlap_white}, 相似度: {custom_similarity:.4f}, 位置: {max_loc}")
+    # 找到所有超过相似度阈值的位置
+    locations = np.where(result >= (white_points * 参数相似度) )
+
+    matches = []
+    for y, x in zip(locations[0], locations[1]):
+        matches.append({
+            'x': x,
+            'y': y,
+            'w': small_w,
+            'h': small_h,
+            'score': result[y, x], # 可以得到这个区域重合的白点个数
+            'count_similarity': 1- (abs(white_points - np.sum(search_mask[y:y+small_h, x:x+small_w]))/(small_w * small_h)) # 可以得到这个区域白点个数相似度
+        })
+    # 找到白点个数相似度最高的点
+    max_item = max(matches, key=lambda x: x['count_similarity'])
+
+    overlap_white = max_item['score']
     
+    custom_similarity = overlap_white / white_points
 
+    print(f"自定义白点匹配率 - 重合白点: {overlap_white}, 小图白点: {white_points}, 个数相似度: {max_item['count_similarity']:.4f}, 重合相似度: {custom_similarity:.4f}, 位置: {max_loc}, 最终相似度: {custom_similarity * 0.8 + max_item['count_similarity'] * 0.2:.4f}")
+
+    # 最终确定的点的相似度,位置计算
     return {
         "x": max_loc[0] + offset_x,
         "y": max_loc[1] + offset_y,
         "w": small_w,
         "h": small_h,
-        "similarity": float(custom_similarity)
+        "similarity": custom_similarity * 0.8 + max_item['count_similarity'] * 0.2
     }
 
 def 找图(large_image_path, small_image_path, region=(0, 0, 0, 0), color_tolerance=None):
